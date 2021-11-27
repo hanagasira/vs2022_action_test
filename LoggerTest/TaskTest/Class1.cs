80749d1c52f8113e7e09876449d5206cea6dc4d5
Rsu <hanagasira2@gmail.com>
2021-11-27 21:56:19 +0900

    set

---
+++ LoggerTest/TaskTest/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+
+namespace TaskTest
+{
+    public class SimpleTask : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            if (Environment.Is64BitProcess)
+            {
+                Console.WriteLine("64-bit process");
+                base.Log.LogMessageFromText($"64-bit process", MessageImportance.High);
+            }
+            else
+            {
+                Console.WriteLine("32-bit process");
+                base.Log.LogMessageFromText($"32-bit process", MessageImportance.High);
+            }
+
+            return true;
+        }
+    }
+}
